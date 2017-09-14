5ae47183f73b863a1dedda494d94b36cbe0b1163
Akash Saravanan <akashsara@outlook.com>
2017-09-14 18:52:26 +0530

    Removed unnecessary files.Changed form function from onclick to onsubmit

--- Hack/login.php
+++ Hack/login.php
@@ -1,7 +1,7 @@
 <?php
-   include 'src/Instamojo.php';
-   $api = new Instamojo\Instamojo('52292c308c428b89fbd26f1320d5bcb4', 'e94f5c790fc8bc18f56b838c83e3cd83');
-  try {
+include 'src/Instamojo.php';
+$api = new Instamojo\Instamojo('52292c308c428b89fbd26f1320d5bcb4', 'e94f5c790fc8bc18f56b838c83e3cd83');
+try {
     $money = ((int)$_POST['option'])*100;
     $response = $api->paymentRequestCreate(array(
         "purpose" => "H.A.C.C 2017",
@@ -10,13 +10,13 @@
         "send_email" => false,
         // "email" => $email,
         "allow_repeated_payments" => false,
-        ));
+    ));
     $pay_url = $response['longurl'];
     header('location:' .$pay_url);
     exit();
-  }
-  catch (Exception $e) {
+}
+catch (Exception $e) {
     print('Error: ' . $e->getMessage());
-  }
-  session_destroy();
+}
+session_destroy();
 ?>
